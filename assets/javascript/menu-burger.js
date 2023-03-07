0872656f7ee92b14b09b9082c47afc3778bb6452
Jonathan.Auber <auber.jonathan@gmail.com>
2023-03-07 11:28:26 +0100

    test de keyframes

---
+++ assets/javascript/menu-burger.js
@@ -0,0 +1,17 @@
+// Menu burger
+
+const NAVBARCONTAINER = document.querySelector(".navbar__link-container");
+const MENUBURGER = document.querySelector(".menu-burger");
+const BURGERTOP = document.querySelector(".burger-top");
+const BURGERMIDDLE = document.querySelector(".burger-middle");
+const BURGERBOTTOM = document.querySelector(".burger-bottom");
+
+function menuBurger() {
+    BURGERTOP.classList.toggle("close-top");
+    BURGERMIDDLE.classList.toggle("off");
+    BURGERBOTTOM.classList.toggle("close-bottom");
+    NAVBARCONTAINER.classList.toggle("display-link-container");
+}
+
+MENUBURGER.addEventListener("click", menuBurger);
+
