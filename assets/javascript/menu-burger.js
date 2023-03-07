560e384f56e5ab19da323bf52258e76d00bb38d9
Jonathan.Auber <auber.jonathan@gmail.com>
2023-03-07 11:33:35 +0100

    push gratuit

--- assets/javascript/menu-burger.js
+++ assets/javascript/menu-burger.js
@@ -6,12 +6,11 @@ const BURGERTOP = document.querySelector(".burger-top");
 const BURGERMIDDLE = document.querySelector(".burger-middle");
 const BURGERBOTTOM = document.querySelector(".burger-bottom");
 
-function menuBurger() {
-    BURGERTOP.classList.toggle("close-top");
-    BURGERMIDDLE.classList.toggle("off");
-    BURGERBOTTOM.classList.toggle("close-bottom");
-    NAVBARCONTAINER.classList.toggle("display-link-container");
+function menuBurger () {
+BURGERTOP.classList.toggle("close-top");
+BURGERMIDDLE.classList.toggle("off");
+BURGERBOTTOM.classList.toggle("close-bottom");
+NAVBARCONTAINER.classList.toggle("display-link-container");
 }
 
-MENUBURGER.addEventListener("click", menuBurger);
-
+MENUBURGER.addEventListener("click",menuBurger);
