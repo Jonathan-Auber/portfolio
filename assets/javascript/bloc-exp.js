92b81c6116e73158691225bd4448cc3249547587
Jonathan.Auber <auber.jonathan@gmail.com>
2023-03-10 11:15:41 +0100

    Page experience desktop ok

--- assets/javascript/bloc-exp.js
+++ assets/javascript/bloc-exp.js
@@ -6,20 +6,64 @@ const POSTE = document.querySelector(".bloc-experiences__left--poste");
 const ARMEE = document.querySelector(".bloc-experiences__left--armee");
 const MOVINGSPAN = document.querySelector(".moving-span");
 
+// Gestion du bloc de droite afficher ou cachant les expériences
+
+const STEFEXP = document.querySelector(".bloc-experiences__right--stef");
+const CANAVESEEXP = document.querySelector(".bloc-experiences__right--canavese");
+const POSTEEXP = document.querySelector(".bloc-experiences__right--poste");
+const ARMEEEXP = document.querySelector(".bloc-experiences__right--armee");
+
+// Les fonctions qui agisse en réponse au click
+
 function stef() {
     MOVINGSPAN.style.top = "0px";
+    STEF.classList.add("gold");
+    CANAVESE.classList.remove("gold");
+    POSTE.classList.remove("gold");
+    ARMEE.classList.remove("gold");
+    STEFEXP.classList.remove("off");
+    CANAVESEEXP.classList.add("off");
+    POSTEEXP.classList.add("off");
+    ARMEEEXP.classList.add("off");
 }
 
 function canavese() {
-    MOVINGSPAN.style.top = "50px";
+    MOVINGSPAN.style.top = "60px";
+    STEF.classList.remove("gold");
+    CANAVESE.classList.add("gold");
+    POSTE.classList.remove("gold");
+    ARMEE.classList.remove("gold");
+    STEFEXP.classList.add("off");
+    CANAVESEEXP.classList.remove("off");
+    POSTEEXP.classList.add("off");
+    ARMEEEXP.classList.add("off");
+
 }
 
 function poste() {
-    MOVINGSPAN.style.top = "100px";
+    MOVINGSPAN.style.top = "120px";
+    STEF.classList.remove("gold");
+    CANAVESE.classList.remove("gold");
+    POSTE.classList.add("gold");
+    ARMEE.classList.remove("gold");
+    STEFEXP.classList.add("off");
+    CANAVESEEXP.classList.add("off");
+    POSTEEXP.classList.remove("off");
+    ARMEEEXP.classList.add("off");
+
 }
 
 function armee() {
-    MOVINGSPAN.style.top = "150px";
+    MOVINGSPAN.style.top = "180px";
+    STEF.classList.remove("gold");
+    CANAVESE.classList.remove("gold");
+    POSTE.classList.remove("gold");
+    ARMEE.classList.add("gold");
+    STEFEXP.classList.add("off");
+    CANAVESEEXP.classList.add("off");
+    POSTEEXP.classList.add("off");
+    ARMEEEXP.classList.remove("off");
+
 }
 
 STEF.addEventListener("click", stef);
